dc618fbd9f037ac456eb203fcc46d346ca6f8ed5
diffficult <spaulo@gmail.com>
2020-07-30 05:08:07 -0300

    ⛅ added new condition emojis

--- .local/bin/xfce4-applets/weather-panel.sh
+++ .local/bin/xfce4-applets/weather-panel.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+#
+# Sunny🌣 🌞 ⛅ 🌤 🌥 ⛱
+# rainy 🌦 🌧 ⛆ 🌢 ☂ ☔ 🌂 
+# snowy 🌨 ☃ ⛄ ⛇ ❄ ❅ ❆
+# windy 🌬 🎏 🎐
+# foggy ☁ 🌫 🌁 
+# stormy 🌩 ⛈ ☇ ☈
+# tornado 🌪 🌀
+# clear ☀ ☼ 🌞 🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘 🌙 ☾ 🌛 🌜 🌝 🌚
+# 
+
 #variable
 #weatherreport="${XDG_DATA_HOME:-$HOME/.local/share}/weatherreport"
 
@@ -12,11 +23,17 @@ readonly ICON="${DIR}/icons/gpu.png"
 # GPU values
 readonly WEATHER_REPORT="$(curl --silent http://wttr.in/$1?T0F)"
 #readonly CURRENT_TEMP="$(curl --silent http://wttr.in/$1 | head $s | cut -c45-46)"
-readonly CURRENT_TEMP="$(curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=1\&locCode\="SAM|AR|AR013|MENDOZA" | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot") 
+readonly CURRENT_TEMP="$(curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=1\&locCode\="SAM|AR|AR013|MENDOZA" | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot") 
 {
-my $sun = "☀️";
+my $sun1 = "☀️";
 binmode(STDOUT, ":utf8");
-print "$sun";
+print "$sun1";
+}
+if( $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny") 
+{
+my $sun2 = "🌤";
+binmode(STDOUT, ":utf8");
+print "$sun2";
 }
 if( $values[0] eq "Mostly Cloudy" || $values[0] eq "Cloudy" || $values[0] eq "Dreary (Overcast)" || $values[0] eq "Fog")
 {
@@ -24,11 +41,29 @@ my $cloud = "⛅";
 binmode(STDOUT, ":utf8");
 print "$cloud";
 }
-if( $values[0] eq "Showers" || $values[0] eq "Mostly Cloudy w/ Showers" || $values[0] eq "Partly Sunny w/ Showers" || $values[0] eq "T-Storms"|| $values[0] eq "Mostly Cloudy w/ T-Storms"|| $values[0] eq "Partly Sunny w/ T-Storms"|| $values[0] eq "Rain")
+if( $values[0] eq "Fog")
 {
-my $rain = "🌧️";
+my $foggy = "🌁";
 binmode(STDOUT, ":utf8");
-print "$rain";
+print "$foggy";
+}
+if( $values[0] eq "Partly Sunny w/ Showers")
+{
+my $rain1 = "🌦";
+binmode(STDOUT, ":utf8");
+print "$rain1";
+}
+if( $values[0] eq "Showers" || $values[0] eq "Mostly Cloudy w/ Showers" || $values[0] eq "Rain")
+{
+my $rain2 = "🌧️";
+binmode(STDOUT, ":utf8");
+print "$rain2";
+}
+if( $values[0] eq "T-Storms" || $values[0] eq "Mostly Cloudy w/ T-Storms"|| $values[0] eq "Partly Sunny w/ T-Storms")
+{
+my $rain3 = "⛈";
+binmode(STDOUT, ":utf8");
+print "$rain3";
 }
 if( $values[0] eq "Windy")
 {
@@ -36,19 +71,25 @@ my $wind = "🌪";
 binmode(STDOUT, ":utf8");
 print "$wind";
 } 
-if($values[0] eq "Flurries" || $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Partly Sunny w/ Flurries"|| $values[0] eq "Snow"|| $values[0] eq "Mostly Cloudy w/ Snow"|| $values[0] eq "Ice"|| $values[0] eq "Sleet"|| $values[0] eq "Freezing Rain"|| $values[0] eq "Rain and Snow"|| $values[0] eq "Cold")
+if( $values[0] eq "Flurries" || $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Partly Sunny w/ Flurries")
 {
-my $snow = "❄️";
+my $flurries = "❄️";
 binmode(STDOUT, ":utf8");
-print "$rain";
+print "$flurries";
 }
-if($values[0] eq "Clear" || $values[0] eq "Mostly Clear" || $values[0] eq "Partly Cloudy"|| $values[0] eq "Intermittent Clouds"|| $values[0] eq "Hazy Moonlight"|| $values[0] eq "Mostly Cloudy"|| $values[0] eq "Partly Cloudy w/ Showers"|| $values[0] eq "Mostly Cloudy w/ Showers"|| $values[0] eq "Partly Cloudy w/ T-Storms"|| $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Mostly Cloudy w/ Snow")
+if( $values[0] eq "Snow"|| $values[0] eq "Mostly Cloudy w/ Snow"|| $values[0] eq "Ice" || $values[0] eq "Sleet" || $values[0] eq "Freezing Rain"|| $values[0] eq "Rain and Snow"|| $values[0] eq "Cold")
 {
-my $night = "🌛";
+my $snowy = "🌨";
+binmode(STDOUT, ":utf8");
+print "$snowy";
+}
+if($values[0] eq "Clear" || $values[0] eq "Mostly Clear" || $values[0] eq "Moonlight" || $values[0] eq "Hazy Moonlight")
+{
+my $night = "🌜";
 binmode(STDOUT, ":utf8");
 print "$night";
 }
-print"$values[1]"; }')"
+$values[1] =~ s/C/ °C/g; print"$values[1]"; }')"
 
 #### Panel
 ###if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
