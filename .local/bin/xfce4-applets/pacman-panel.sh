ae9ee0b10e3822fc4826308ddfc132bcf8016a66
diffficult <spaulo@gmail.com>
2020-07-31 02:13:31 -0300

    📦 package panel updated - cleaned a bit the shitty code, learned a couple of lessons

--- .local/bin/xfce4-applets/pacman-panel.sh
+++ .local/bin/xfce4-applets/pacman-panel.sh
@@ -10,43 +10,46 @@ readonly DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #readonly ICON="${DIR}/icons/package-manager/pacman.svg"
 readonly ICON="${DIR}/icons/package-manager/pacman3.png"
 readonly ICONOK="${DIR}/icons/package-manager/packageok.png"
+
 # Calculate updates
 readonly AUR=$(yay -Qua | wc -l)
 readonly OFFICIAL=$(checkupdates | wc -l)
 readonly ALL=$(( AUR + OFFICIAL ))
 
-# Panel
-if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
-  INFO="<img>${ICON}</img>"
-  INFO+="<txt>"
-else
-  INFO="<txt>"
-  INFO+="Icon is missing!"
-fi
-INFO+=" ${ALL} "
-INFO+="</txt>"
-
-# Tooltip
-#MORE_INFO="<tool>"
-#MORE_INFO+="┌─ 📦 Updates Available\n"
-#MORE_INFO+="├─ ${OFFICIAL}  from repos\n"
-#MORE_INFO+="└─ ${AUR}  from AUR"
-#MORE_INFO+="</tool>"
-
-# Panel Print
+# Panel and Tooltip display
 if [[ ${ALL} -eq "0" ]]; then
-  echo -e "<img>${ICONOK}</img>"
-  MORE_INFO="<tool>"
-  MORE_INFO+=" ✅ Packages up to date"
-  MORE_INFO+="</tool>"
+  if [[ $(file -b "${ICONOK}") =~ PNG|SVG ]]; then
+    INFO="<img>${ICONOK}</img><click>pamac-manager</click>"
+  else
+  INFO="<txt>⚠️</txt>"
+  fi
+  MORE_INFO="<tool> ✅ Packages up to date </tool>"
 else
-  echo -e "${INFO}"
+  if [[ $(file -b "${ICON}") =~ PNG|SVG ]]; then
+     INFO="<img>${ICON}</img><click>pamac-manager</click><txt>"
+  else
+     INFO="<txt>⚠️"
+  fi
+  INFO+=" ${ALL} "
+  INFO+="</txt>"
   MORE_INFO="<tool>"
   MORE_INFO+="┌─ ⚠️ Updates Available\n"
   MORE_INFO+="├─ ${OFFICIAL}  from repos\n"
   MORE_INFO+="└─ ${AUR}  from AUR"
   MORE_INFO+="</tool>"
 fi
+
+# Tooltip (old simple)
+#MORE_INFO="<tool>"
+#MORE_INFO+="┌─ 📦 Updates Available\n"
+#MORE_INFO+="├─ ${OFFICIAL}  from repos\n"
+#MORE_INFO+="└─ ${AUR}  from AUR"
+#MORE_INFO+="</tool>"
+
+
+# Panel Print
+echo -e "${INFO}"
+
 
 # Tooltip Print
 echo -e "${MORE_INFO}"
