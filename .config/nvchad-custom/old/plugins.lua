6fcf6d14c96f346a1ce7bed5832b1a70c1594cce
diffficult <spaulo@gmail.com>
2023-10-10 02:51:26 -0300

    🔨 tweaking neovim+nvchad

--- .config/nvchad-custom/old/plugins.lua
+++
@@ -1,6 +0,0 @@
-local plugins = {
---  { "MaximilianLloyd/ascii.nvim" , lazy = false },
---  { "MunifTanjim/nui.nvim" , lazy = false }
---  { "MaximilianLloyd/ascii.nvim", requires = { "MunifTanjim/nui.nvim" } }
-}
-return plugins
--- .config/nvchad-custom/plugins.lua
+++ .config/nvchad-custom/plugins.lua
@@ -32,7 +32,53 @@ local plugins = {
     },
     config = function() end,
   },
-
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    enabled = false,
+  },
+  {
+    "folke/which-key.nvim",
+    enabled = true,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = function()
+      return require("plugins.configs.others").gitsigns
+    end,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "git")
+      require("scrollbar.handlers.gitsigns").setup()
+      require("gitsigns").setup(opts)
+    end,
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    opts = overrides.devicons,
+  },
+  {
+    "NvChad/nvterm",
+    opts = overrides.nvterm,
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = overrides.telescope,
+    dependencies = {
+      "debugloop/telescope-undo.nvim",
+      "gnfisher/nvim-telescope-ctags-plus",
+      "tom-anders/telescope-vim-bookmarks.nvim",
+      "benfowler/telescope-luasnip.nvim",
+      "nvim-telescope/telescope-dap.nvim",
+      "Marskey/telescope-sg",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      {
+        "nvim-telescope/telescope-frecency.nvim",
+        dependencies = { "kkharji/sqlite.lua" },
+      },
+    },
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     dependencies = {
@@ -88,83 +134,6 @@ local plugins = {
     "NvChad/nvim-colorizer.lua",
     opts = overrides.colorizer,
   },
---  {
---    "hrsh7th/nvim-cmp",
---    opts = cmp_opt.cmp,
---    dependencies = {
---      "delphinus/cmp-ctags",
---      "hrsh7th/cmp-nvim-lsp-document-symbol",
---      "ray-x/cmp-treesitter",
---      "tzachar/cmp-fuzzy-buffer",
---      "roobert/tailwindcss-colorizer-cmp.nvim",
---      "tzachar/fuzzy.nvim",
---      "rcarriga/cmp-dap",
---      "js-everts/cmp-tailwind-colors",
---      { "jcdickinson/codeium.nvim", config = true },
---      {
---        "tzachar/cmp-tabnine",
---        build = "./install.sh",
---        config = function()
---          local tabnine = require "cmp_tabnine.config"
---          tabnine:setup {
---            max_lines = 1000,
---            max_num_results = 3,
---            sort = true,
---            show_prediction_strength = false,
---            run_on_every_keystroke = true,
---            snipper_placeholder = "..",
---            ignored_file_types = {},
---          }
---        end,
---      },
---      {
---        "L3MON4D3/LuaSnip",
---        build = "make install_jsregexp",
---        config = function(_, opts)
---          require("plugins.configs.others").luasnip(opts)
---          require "custom.configs.luasnip"
---          require "custom.configs.autotag"
---        end,
---      },
---      {
---        "windwp/nvim-autopairs",
---        config = function()
---          require "custom.configs.autopair"
---        end,
---      },
---    },
---    config = function(_, opts)
---      dofile(vim.g.base46_cache .. "cmp")
---      local format_kinds = opts.formatting.format
---      opts.formatting.format = function(entry, item)
---        if item.kind == "Color" then
---          item.kind = "⬤"
---          format_kinds(entry, item)
---          return require("tailwindcss-colorizer-cmp").formatter(entry, item)
---        end
---        return format_kinds(entry, item)
---      end
---      local cmp = require "cmp"
---
---      cmp.setup(opts)
---
---      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
---      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
---
---      cmp.setup.cmdline({ "/", "?" }, {
---        mapping = opts.mapping,
---        sources = {
---          { name = "buffer" },
---        },
---      })
---
---      cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
---        sources = {
---          { name = "dap" },
---        },
---      })
---    end,
---  },
   {
     "karb94/neoscroll.nvim",
     keys = { "<C-d>", "<C-u>" },
@@ -782,6 +751,126 @@ local plugins = {
       notificationOnAutoClose = false,
     },
   },
+---- code completion nvim-cmp ------
+  {
+    "hrsh7th/nvim-cmp",
+    -- opts = cmp_opt.cmp,
+    dependencies = {
+      "delphinus/cmp-ctags",
+      "hrsh7th/cmp-nvim-lsp-document-symbol",
+      "hrsh7th/cmp-copilot",
+      "ray-x/cmp-treesitter",
+      "tzachar/cmp-fuzzy-buffer",
+      "roobert/tailwindcss-colorizer-cmp.nvim",
+      "tzachar/fuzzy.nvim",
+      "rcarriga/cmp-dap",
+      "js-everts/cmp-tailwind-colors",
+      { "jcdickinson/codeium.nvim", config = true },
+      {
+        "tzachar/cmp-tabnine",
+        build = "./install.sh",
+        config = function()
+          local tabnine = require "cmp_tabnine.config"
+          tabnine:setup {
+            max_lines = 1000,
+            max_num_results = 3,
+            sort = true,
+            show_prediction_strength = false,
+            run_on_every_keystroke = true,
+            snipper_placeholder = "..",
+            ignored_file_types = {},
+          }
+        end,
+      },
+      {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
+        config = function(_, opts)
+          require("plugins.configs.others").luasnip(opts)
+          require "custom.configs.luasnip"
+          require "custom.configs.autotag"
+        end,
+      },
+      {
+        "windwp/nvim-autopairs",
+        config = function()
+          require "custom.configs.autopair"
+        end,
+      },
+      {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        dependencies = {
+          {
+            "zbirenbaum/copilot-cmp",
+            config = function()
+              require("copilot_cmp").setup()
+            end,
+          },
+        },
+        config = function()
+          require("copilot").setup {
+            suggestion = {
+              enabled = false,
+              auto_trigger = false,
+              keymap = {
+                accept_word = false,
+                accept_line = false,
+              },
+            },
+            panel = {
+              enabled = false,
+            },
+            filetypes = {
+              gitcommit = false,
+              TelescopePrompt = false,
+            },
+            server_opts_overrides = {
+              trace = "verbose",
+              settings = {
+                advanced = {
+                  listCount = 3,
+                  inlineSuggestCount = 3,
+                },
+              },
+            },
+          }
+        end,
+      },
+    },
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "cmp")
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        if item.kind == "Color" then
+          item.kind = "⬤"
+          format_kinds(entry, item)
+          return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+        end
+        return format_kinds(entry, item)
+      end
+      local cmp = require "cmp"
+
+      cmp.setup(opts)
+
+      local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = opts.mapping,
+        sources = {
+          { name = "buffer" },
+        },
+      })
+
+      cmp.setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+        sources = {
+          { name = "dap" },
+        },
+      })
+    end,
+  },
+
   -- Install a plugin
   {
     "max397574/better-escape.nvim",
